2092ec783400549e3110fba7868eddcb837fa9dc
warlord200 <thedarkpaladin05@gmail.com>
2022-02-12 12:11:39 +0800

    Solved solution 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
+const ftoc = function(F) {
+  let C;
+  let number;
+  number = (F - 32) * 5/9
+  C = Math.round(number * 10) / 10
+  return C;
 
 };
 
-const ctof = function() {
+const ctof = function(C) {
+  let F;
+  let number;
+  number = (C * 9/5) + 32
+  F = Math.round(number * 10) / 10
+  return F;
 
 };
 
