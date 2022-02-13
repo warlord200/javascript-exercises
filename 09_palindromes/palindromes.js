526e93296963d744522c3de438b55649d5e5a538
warlord200 <thedarkpaladin05@gmail.com>
2022-02-14 01:10:43 +0800

    Finished solution 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (stringInput) {
+    let stringNoPunc = stringInput.toLowerCase().replace(/ /g, "").replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g,"");
+    let stringInputArray = stringNoPunc.split('');
+    let newString = []
+    for(let i = stringInputArray.length - 1; i >= 0;i-- ){
+        newString.push(stringInputArray[i])
+    }
+    return JSON.stringify(stringInputArray) === JSON.stringify(newString);
 
 };
 
