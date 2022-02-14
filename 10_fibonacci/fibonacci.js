1ece5e8751ba7406b7debcb7a37618c8e3866f4b
warlord200 <thedarkpaladin05@gmail.com>
2022-02-14 10:21:00 +0800

    Finished solution 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num <0 ){
+        return "OOPS"
+    }else{}
+    let fiboSeq = [0]
+    for(let a = 0; a <= num; a++){
+        if(a == 0){
+            fiboSeq.push(a + 1)
+
+        }else{
+            fiboSeq.push(fiboSeq[a] + fiboSeq[a -1])
+        }
+    }
+    return fiboSeq[num]
+
 
 };
 
