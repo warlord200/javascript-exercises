c935d3f8f1c9cda992528e4903171cf345babee5
warlord200 <thedarkpaladin05@gmail.com>
2022-02-15 13:40:52 +0800

    Finished solution 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(list) {
+    let reformList = list.map((item)=>{return item['title'];} )
+    return reformList;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
